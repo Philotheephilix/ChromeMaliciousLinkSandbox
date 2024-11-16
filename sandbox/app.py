70b5d7c397e0fcaa65d2f2859e08dcc9e35b2fb6
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-11-17 02:01:51 +0530

    organized with start and stop script

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,11 +1,43 @@
 from flask import Flask, render_template
-import os
+import subprocess
+
 app = Flask(__name__)
 
-@app.route('/initialize-docker')
-def home():
-    os.system('docker run -d     --name=firefox     -p 5800:5800     -v /docker/appdata/firefox:/config:rw     jlesage/firefox ')
-    return ' hello'
+@app.route('/initialize-docker/<URL>')
+def UrlToDocker(URL):
+    try:
+        command = [
+            "docker", "run", "-d", "--name=firefox",
+            "-p", "5800:5800",
+            "-v", "/docker/appdata/firefox:/config:rw",
+            "-e", f"FF_OPEN_URL={URL}",
+            "jlesage/firefox"
+        ]
+        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        output = f"STDOUT:\n{result.stdout}\n\nSTDERR:\n{result.stderr}"
+    except Exception as e:
+        output = f"Error occurred: {str(e)}"
+    
+    return output
 
+@app.route('/stop-sandbox')
+def DockerShutdown():
+    try:
+        DStop = [
+            "docker", "stop", "firefox",
+        ]
+        DRemove = [
+            "docker", "rm", "firefox"
+        ]
+        StopStatus = subprocess.run(DStop, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        RemoveStatus = subprocess.run(DRemove, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+        StopOutput = f"STDOUT:\n{StopStatus.stdout}\n\nSTDERR:\n{StopStatus.stderr}"
+        RemoveOutput = f"STDOUT:\n{RemoveStatus.stdout}\n\nSTDERR:\n{RemoveStatus.stderr}"
+    except Exception as e:
+        output = f"Error occurred: {str(e)}"
+    
+    return StopOutput,RemoveOutput
 if __name__ == '__main__':
-    app.run(debug=True,port=5900)
+    app.run(debug=True, port=5900)
