170ddcd4387fbf479602fd2c178dd86e0ef71b8a
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-12-03 19:56:27 +0530

    sandbox made working

--- extension/popup.js
+++ extension/popup.js
@@ -1,20 +1,22 @@
-// popup.js
-
-// Get the elements we need to work with
-const currentUrlInput = document.getElementById("current-url");
-const openUrlInput = document.getElementById("open-url");
-const openButton = document.getElementById("open-button");
-
-// Function to get the current tab's URL
-chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  const currentUrl = tabs[0].url;
-  currentUrlInput.value = currentUrl;  // Display current URL in the input
+document.addEventListener('DOMContentLoaded', () => {
+  const openLinkButton = document.getElementById('openLinkBtn');
+  openLinkButton.addEventListener('click', createInstance);
+  const purgeButton = document.getElementById('purgeBtn');
+  purgeButton.addEventListener('click', purgeInstance);
 });
 
-// Add event listener for the "Open" button
-openButton.addEventListener('click', () => {
-  const urlToOpen = openUrlInput.value.trim();
-  if (urlToOpen) {
-    chrome.tabs.create({ url: urlToOpen });
-  }
-});
+function createInstance() {
+    const link = document.getElementById('linkInput').value;
+    fetch(`http://localhost:5900/initialize-docker/${link}`)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    document.getElementById('sandbox').innerHTML = `<iframe style="width: 100vw; height: 100vh;" src="http://localhost:5800/" title="Webpage Inside Extension"></iframe>`;
+}
+
+function purgeInstance(){
+  fetch('http://localhost:5900/stop-sandbox')
+}
